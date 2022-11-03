fd19b41356ad050ee1161f373b8e16e2c484fc7c
CASPIAN76 <thakren76@gmail.com>
2022-11-03 12:29:11 +0530

    update

---
+++ Frontend/src/app/shareModule/angular-material/angular-material.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+
+const Material:any=[]
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    Material
+  ],
+  exports:[Material]
+})
+export class AngularMaterialModule { }
